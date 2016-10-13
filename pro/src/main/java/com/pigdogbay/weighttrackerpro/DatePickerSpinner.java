b5fd4761c4f2a49de2df27951b7fa1ed2bd562c3
Mark Bailey <pigdogbay@yahoo.co.uk>
2016-10-13 11:54:38 +0100

    DatPickerSpinner now looks like CustomNumberPicker and stores/restores date on rotations

--- pro/src/main/java/com/pigdogbay/weighttrackerpro/DatePickerSpinner.java
+++ pro/src/main/java/com/pigdogbay/weighttrackerpro/DatePickerSpinner.java
@@ -2,12 +2,17 @@ package com.pigdogbay.weighttrackerpro;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Context;
 import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -15,8 +20,20 @@ import android.widget.LinearLayout;
 
 public class DatePickerSpinner extends LinearLayout implements
 		OnDateSetListener {
-	Button _DisplayButton;
-	Calendar _Calendar;
+	/**
+	 * Identifier for the state to save the selected index of
+	 * the side spinner.
+	 */
+	private static String STATE_TIME = "Time";
+	private static String STATE_TIME_ZONE = "TimeZone";
+
+	/**
+	 * Identifier for the state of the super class.
+	 */
+	private static String STATE_SUPER_CLASS = "SuperClass";
+
+	private Button _DisplayButton;
+	private Calendar _Calendar;
 
 	public Calendar getCalendar() {
 		return _Calendar;
@@ -34,50 +51,33 @@ public class DatePickerSpinner extends LinearLayout implements
 	public DatePickerSpinner(Context context, AttributeSet attributeSet) {
 		super(context, attributeSet);
 		this.setOrientation(HORIZONTAL);
+		LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.date_picker_spinner,this);
+	}
 
-		Button minusButton = new Button(context);
-		minusButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				minusButtonClicked();
-			}
-		});
-		minusButton.setText("-");
-		minusButton.setMinEms(2);
-		minusButton.setTextAppearance(context,
-				android.R.style.TextAppearance_Large);
-		minusButton.setTypeface(null, Typeface.BOLD);
-		LinearLayout.LayoutParams minBtnLayParams = new LayoutParams(
-				LinearLayout.LayoutParams.WRAP_CONTENT,
-				LinearLayout.LayoutParams.WRAP_CONTENT);
-
-		Button plusButton = new Button(context);
-		plusButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				plusButtonClicked();
-			}
-		});
-		plusButton.setText("+");
-		plusButton.setMinEms(2);
-		plusButton.setTextAppearance(context,
-				android.R.style.TextAppearance_Large);
-		plusButton.setTypeface(null, Typeface.BOLD);
-
-		_DisplayButton = new Button(context);
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+		_DisplayButton = (Button) this.findViewById(R.id.pickerSetBtn);
 		_DisplayButton.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v) {
+			public void onClick(View view) {
 				displayButtonClicked();
 			}
 		});
-		_DisplayButton.setMinEms(6);
-		_DisplayButton.setTextAppearance(context,
-				android.R.style.TextAppearance_Large);
+		this.findViewById(R.id.pickerMinusBtn).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				minusButtonClicked();
+			}
+		});
 
-		addView(minusButton, minBtnLayParams);
-		addView(_DisplayButton, minBtnLayParams);
-		addView(plusButton, minBtnLayParams);
+		this.findViewById(R.id.pickerPlusBtn).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				plusButtonClicked();
+			}
+		});
 		setCalendar(Calendar.getInstance());
 	}
 
@@ -109,5 +109,42 @@ public class DatePickerSpinner extends LinearLayout implements
 		_Calendar.set(year, monthOfYear, dayOfMonth);
 		updateDisplay();
 	}
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		Bundle bundle = new Bundle();
+		bundle.putParcelable(STATE_SUPER_CLASS,super.onSaveInstanceState());
+		bundle.putLong(STATE_TIME,_Calendar.getTimeInMillis());
+		bundle.putString(STATE_TIME_ZONE,_Calendar.getTimeZone().getID());
+		return bundle;
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+		if (state instanceof Bundle){
+			Bundle bundle = (Bundle)state;
+			super.onRestoreInstanceState(bundle.getParcelable(STATE_SUPER_CLASS));
+			_Calendar.setTimeZone(TimeZone.getTimeZone(bundle.getString(STATE_TIME_ZONE)));
+			_Calendar.setTimeInMillis(bundle.getLong(STATE_TIME));
+			updateDisplay();
+		}else {
+			super.onRestoreInstanceState(state);
+		}
+	}
+	@Override
+	protected void dispatchSaveInstanceState(SparseArray<Parcelable> container) {
+		// Makes sure that the state of the child views in the side
+		// spinner are not saved since we handle the state in the
+		// onSaveInstanceState.
+		super.dispatchFreezeSelfOnly(container);
+	}
+
+	@Override
+	protected void dispatchRestoreInstanceState(SparseArray<Parcelable> container) {
+		// Makes sure that the state of the child views in the side
+		// spinner are not restored since we handle the state in the
+		// onSaveInstanceState.
+		super.dispatchThawSelfOnly(container);
+	}
+
 
 }

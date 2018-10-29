cacb82daf7cab9619777727e9f5f4332fff85b8a
mark <mpdbailey.technology@gmail.com>
2018-10-29 11:42:04 +0000

    Updated gradle files

--- pro/src/weightTrackerStd/java/com/pigdogbay/weightrecorder/utils/AdsVariation.java
+++ pro/src/weightTrackerStd/java/com/pigdogbay/weightrecorder/utils/AdsVariation.java
@@ -1,9 +1,12 @@
 package com.pigdogbay.weightrecorder.utils;
 
+import android.os.Bundle;
 import android.view.View;
 
+import com.google.ads.mediation.admob.AdMobAdapter;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.pigdogbay.weighttrackerpro.R;
 
 public class AdsVariation implements IAds {
@@ -26,17 +29,20 @@ public class AdsVariation implements IAds {
 
     @Override
     public void setUp(View view) {
+        MobileAds.initialize(view.getContext(), "ca-app-pub-3582986480189311~6146792389");
         // Look up the AdView as a resource and load a request.
-        _AdView = (AdView) view.findViewById(R.id.adView);
+        _AdView = view.findViewById(R.id.adView);
+        Bundle extras = new Bundle();
+        extras.putString("max_ad_content_rating", "G");
+        //MA = Mature Adult, may improve eCPM?
+        //extras.putString("max_ad_content_rating", "MA");
+
         AdRequest adRequest = new AdRequest.Builder()
+                .addNetworkExtrasBundle(AdMobAdapter.class, extras)
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .addTestDevice(view.getResources().getString(R.string.code_test_device_1_id))
-                .addTestDevice(view.getResources().getString(R.string.code_test_device_2_id))
-                .addTestDevice(view.getResources().getString(R.string.code_test_device_3_id))
-                .addTestDevice(view.getResources().getString(R.string.code_test_device_4_id))
-                .addTestDevice(view.getResources().getString(R.string.code_test_device_5_id))
-                .addTestDevice(view.getResources().getString(R.string.code_test_device_6_id))
-                .addTestDevice(view.getResources().getString(R.string.code_test_device_7_id))
+                .addTestDevice(view.getResources().getString(R.string.code_test_device_acer_tablet))
+                .addTestDevice(view.getResources().getString(R.string.code_test_device_moto_g))
+                .addTestDevice(view.getResources().getString(R.string.code_test_device_nokia_6))
                 .build();
         _AdView.loadAd(adRequest);
 
